a4d3232b32d9199a549047825c4113a484f3dc01
Smruti Mahapatra <getsmruti03@gmail.com>
2025-01-25 18:51:59 +0530

    Feat: Added the Not Implemented status code error in the controller APIs

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -3,23 +3,23 @@ function pingProblemController(req, res) {
 }
 
 function addProblem(req, res) {
-    
+    res.status(501).json({message: 'Not implemented'});
 }
 
 function getProblem(req, res) {
-    
+    res.status(501).json({message: 'Not implemented'});
 }
 
 function getProblems(req, res) {
-    
+    res.status(501).json({message: 'Not implemented'});
 }
 
 function deleteProblem(req, res) {
-    
+    res.status(501).json({message: 'Not implemented'});
 }
 
 function updateProblem(req, res) {
-    
+    res.status(501).json({message: 'Not implemented'});
 }
 
 module.exports = {
