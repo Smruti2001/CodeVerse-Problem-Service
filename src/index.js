5f1f38e2d85930ef7fe5fffa7c9284527b145979
Smruti Mahapatra <getsmruti03@gmail.com>
2025-01-27 01:02:20 +0530

    Feat: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/server.config');
 const apiRouter = require('./routes');
 const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.get('/ping', (req, res) => {
 // The custom error handler middleware 
 app.use(errorHandler)
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server started listening at PORT ${PORT}`);
+    await connectToDB();
+    console.log('Successfully connected to DB server.')
 })
